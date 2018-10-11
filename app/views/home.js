e4ba2cdee68b1196c66789f05f18c0f136df477b
Luke Andersen <luke@lawebdesign.com.au>
2018-10-12 10:45:14 +1100

    Space fix

--- app/views/home.js
+++ app/views/home.js
@@ -8,8 +8,7 @@ const Home = () => (
             <h4>We&rsquo;re here to assist Australian people effected by brain cancer in a unique way!</h4>
             <p>The Killer B Foundation is a non-for-profit organisation that takes a very unique approach to fundraising. The Killer B Foundation holds an annual event known as the &ldquo;Annual Postie Bike Challenge&rdquo; or also known as the &ldquo;APBC&rdquo; where all participants ride Postie Bikes embarking on crazy adventures, crossing state lines and camping whilst raising vital needed funds and having a blast along the way!</p>
             <p>All funds raised by The Killer B Foundation go directly to people affected by brain cancer. We do not put funds into brain cancer research; we invest the money into local communities to assist in upgrading of facilities, providing transport for patients or providing funds to patients that simply cant afford the treatment they deserve.</p>
-            <p>For any questions or enquires please <a href=
-"mailto:admin@killerbfoundation.org">contact us</a>.</p>
+            <p>For any questions or enquires please <a href="mailto:admin@killerbfoundation.org">contact us</a>.</p>
 
             <a href="mailto:admin@killerbfoundation.org" className="btn btn-primary m-t-2 m-b-4">Contact Us</a>
 
