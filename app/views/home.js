3419f1c901a8f30646d4f5241e4314fac66c89e7
Luke Andersen <luke@lawebdesign.com.au>
2018-10-12 10:33:51 +1100

    Update copy

--- app/views/home.js
+++ app/views/home.js
@@ -4,9 +4,12 @@ const Home = () => (
     <div className="container row">
         <div className="col-xs-12 col-md-8">
 
-            <h2>Get ready for an amazing adventure.</h2>
-            <h4>You might loose a few, but you&rsquo;ll save a lot.</h4>
-            <p>Lorem ipsum dolor sit amet, <strong>consectetur adipisicing</strong> elit, sed do eiusmod <em>tempor incididunt</em> ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in <a href="#link">voluptate velit esse</a> cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h2>THE KILLER B: BRAIN CANCER</h2>
+            <h4>We&rsquo;re here to assist Australian people effected by brain cancer in a unique way!</h4>
+            <p>The Killer B Foundation is a non-for-profit organisation that takes a very unique approach to fundraising. The Killer B Foundation holds an annual event known as the &ldquo;Annual Postie Bike Challenge&rdquo; or also known as the &ldquo;APBC&rdquo; where all participants ride Postie Bikes embarking on crazy adventures, crossing state lines and camping whilst raising vital needed funds and having a blast along the way!</p>
+            <p>All funds raised by The Killer B Foundation go directly to people affected by brain cancer. We do not put funds into brain cancer research; we invest the money into local communities to assist in upgrading of facilities, providing transport for patients or providing funds to patients that simply cant afford the treatment they deserve.</p>
+            <p>For any questions or enquires please <a href=
+"mailto:admin@killerbfoundation.org">contact us</a>.</p>
 
             <a href="mailto:admin@killerbfoundation.org" className="btn btn-primary m-t-2 m-b-4">Contact Us</a>
 
